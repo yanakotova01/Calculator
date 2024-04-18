8c8103b3c3493db7e07d19b72199c0d5391fad1e
yanakotova01 <voadng@gmail.com>
2024-04-18 12:56:21 +0300

    функция суммы сделана

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float one, two, result;
+            char sign;
+            Console.WriteLine("Добро пожаловать в калькулятор. Вам нелбходимо ввести первое число, затем знак действия(+,-,*,/), которое хотите совершить и второе число.");
+            Console.Write("Введите первое число: ");
+            one = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите знак действия : ");
+            sign = Convert.ToChar(Console.ReadLine());
+            Console.Write("Введите Второе число: ");
+            two = Convert.ToSingle(Console.ReadLine());
+            if (sign == '+')
+            {
+                result = one + two;
+                Console.WriteLine("Сумма ваших чисел равна " + result);
+                Console.WriteLine("Для выхода нажмите любую клавишу...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
