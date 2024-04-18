d6f4661580bfd7fe0172a4a921f7f074a3dfe687
yanakotova01 <voadng@gmail.com>
2024-04-18 13:06:15 +0300

    функция разности сделана

--- Program.cs
+++ Program.cs
@@ -27,6 +27,13 @@ namespace calculator
                 Console.WriteLine("Для выхода нажмите любую клавишу...");
                 Console.ReadKey();
             }
+            else if (sign == '-') 
+            {
+              result = one - two;
+              Console.WriteLine("Разность ваших чисел равна " + result);
+              Console.WriteLine("Для выхода нажмите любую клавишу...");
+              Console.ReadKey();
+            }
         }
     }
 }
